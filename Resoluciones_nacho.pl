b1cf7c05e2de01dcc34e557be1b2ed16992dd5d8
nachoberaza <nachoberaza@gmail.com>
2022-09-02 18:16:59 -0300

    Agrego las resoluciones de todos los puntos.

--- Resoluciones_nacho.pl
+++ Resoluciones_nacho.pl
@@ -1,5 +1,66 @@
-% 3. Puntos Inventados 
-----------------------------------------------------------------------------------------
+:- discontiguous entre7y10Puntos/1.
+
+temaCopado(chocobos).
+temaCopado(inversiones).
+temaCopado(harryPotter).
+temaCopado(carpinchos).
+temaCopado(bidones).
+temaCopado(piratas).
+temaCopado(tierraMedia).
+temaCopado(kraken).
+
+punto(primerOrden).
+punto(ordenSuperior).
+punto(polimorfismo).
+punto(listas).
+punto(relleno).
+punto(modelado).
+punto(diagramado).
+
+% parcial( Tema, [Puntos] ).
+parcial(inversiones,[polimorfismo,polimorfismo,listas,relleno,modelado,diagramado,primerOrden,ordenSuperior]).
+
+% sabe(Alumno,Punto)
+sabe(nacho,polimorfismo).
+sabe(nacho,relleno).
+sabe(nacho,primerOrden).
+sabe(nacho,ordenSuperior).
+sabe(nacho,modelado).
+sabe(nacho,listas).
+sabe(nacho,diagramado).
+
+
+sabe(nicolas,diagramado).
+sabe(francisco,relleno).
+sabe(francisco,listas).
+
+
+/* --------------------------------------------------------------------------------------------------------------------------------- */
+
+% 1. Parcial Evalua ----------------------------------------------------------------------------------------
+parcialEvalua( parcial(_,Puntos) , PuntoQueEvalua):-
+    punto(PuntoQueEvalua),
+    member(PuntoQueEvalua,Puntos).
+
+% 2. Parcial Listo ----------------------------------------------------------------------------------------
+todosLosPuntos(TodosLosPuntosExistentes):-
+    findall(P,punto(P),TodosLosPuntosExistentes).
+
+entre7y10Puntos(Puntos):-
+    length(Puntos, Largo),
+    between(7, 10, Largo).
+    
+evaluaTodo(PuntosEvaluados):-
+    todosLosPuntos(PuntosAEvaluar),
+    subset(PuntosAEvaluar,PuntosEvaluados).
+
+esParcialListo( parcial(Tema,Puntos) ):-
+    entre7y10Puntos(Puntos),
+    evaluaTodo(Puntos),
+    temaCopado(Tema).
+
+
+% 3. Puntos Inventados ----------------------------------------------------------------------------------------
 tomadoAlAzar(PuntoAleatorio):-
     todosLosPuntos(PE),
     length(PE, TotalDePuntos), 
@@ -28,8 +89,38 @@ puntosInventados(ListaDePuntos):-
     
 
 
-% 4. Nuevo Parcial 
-----------------------------------------------------------------------------------------
+% 4. Nuevo Parcial ----------------------------------------------------------------------------------------
 nuevoParcial(parcial(Tema,Puntos)):-
     puntosInventados(Puntos),
     esParcialListo(parcial(Tema,Puntos)).
+
+% 6. Aprueba ----------------------------------------------------------------------------------------
+todosLosPuntosQueSabe(Estudiante,PuntosQueSabe):-
+    sabe(Estudiante,_),
+    sabe(OtroE,_),
+    Estudiante \= OtroE,
+    findall(PA,sabe(Estudiante,PA),PuntosQueSabe).
+    
+aprueba(Estudiante,parcial(_,PuntosATomar)):-
+    todosLosPuntosQueSabe(Estudiante,PuntosQueSabe),
+    subset(PuntosATomar,PuntosQueSabe).
+
+% 7. Los que aprueban ----------------------------------------------------------------------------------------
+
+% findall(Estudiante,aprueba(Estudiante,parcial(chocobos,[primerOrden,primerOrden,ordenSuperior,polimorfismo,listas,relleno,modelado,diagramado])),Aprobados).
+
+
+% 8. Solo para elegidos (Utilizando la estructura de la consulta del punto 7) ----------------------------------------------------------------------------------------
+todosSonElMismo(Lista):-
+    forall(
+        (
+            member(Elem1, Lista),
+            member(Elem2,Lista)
+        ),
+        Elem1 == Elem2   
+    ).
+
+soloParaElegidos(Parcial):-
+    findall( Estudiante, aprueba( Estudiante,Parcial),Aprobados),
+    todosSonElMismo(Aprobados).
+    
